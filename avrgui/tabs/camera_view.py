0e8c08c516e717a92c4542bf65ce8878f26ad211
literallyanything <hunterbaker@me.com>
2022-10-08 09:38:12 -0400

    Add auto selection, toasts, and menus to camera_view.py

--- avrgui/tabs/camera_view.py
+++ avrgui/tabs/camera_view.py
@@ -1,12 +1,15 @@
+import json
 import socket
 from threading import Thread
 
 import numpy as np
-from PySide6 import QtCore, QtWidgets
+from PySide6 import QtCore, QtGui, QtWidgets
 from loguru import logger
 
 from avrgui.lib import stream
+from avrgui.lib.enums import ConnectionState
 from avrgui.lib.graphics_label import GraphicsLabel
+from avrgui.lib.toast import Toast
 from avrgui.lib.widgets import IntLineEdit
 from avrgui.tabs.base import BaseTabWidget
 from avrgui.tabs.connection.mqtt import MQTTConnectionWidget
@@ -55,9 +58,15 @@ class CameraViewWidget(BaseTabWidget):
     streaming_changed = QtCore.Signal(bool)
     send_status_message = QtCore.Signal(str)
 
-    def __init__(self, parent: QtWidgets.QWidget) -> None:
+    def __init__(self, parent: QtWidgets.QWidget, toast: Toast) -> None:
         super().__init__(parent)
 
+        # self.video_menu_cameras = {}
+        # self.video_menu_camera = None
+        self.auto_select_check = None
+        self.video_menu_auto = None
+        self.video_menu_streaming = None
+        self.video_menu = None
         self.shutting_down = False
         self._disconnect_toast = None
         self._connect_toast = None
@@ -72,6 +81,8 @@ class CameraViewWidget(BaseTabWidget):
         self.streaming_text = None
         self.model_text = None
         self.view = None
+        self.toast = toast
+        self.auto_select = False
 
         self.setWindowTitle("Camera View")
 
@@ -108,12 +119,18 @@ class CameraViewWidget(BaseTabWidget):
         self.camera_picker = QtWidgets.QComboBox()
         for camera in CAMERAS:
             self.camera_picker.addItem(camera)
-        self.camera_picker.currentTextChanged.connect(self.camera_selected)
+        self.camera_picker.currentTextChanged.connect(self.set_camera)
         options_layout.addRow("Camera", self.camera_picker)
 
         self.port_line_edit = IntLineEdit()
         self.port_line_edit.setText("9999")
         options_layout.addRow(QtWidgets.QLabel("Port: "), self.port_line_edit)
+
+        self.auto_select_check = QtWidgets.QCheckBox("Auto Select")
+        self.auto_select_check.setChecked(False)
+        self.auto_select_check.stateChanged.connect(self.set_auto_select)
+        options_layout.addWidget(self.auto_select_check)
+
         self.connect_button = QtWidgets.QPushButton("Connect")
         self.connect_button.clicked.connect(lambda: self.set_streaming())
         options_layout.addWidget(self.connect_button)
@@ -146,20 +163,41 @@ class CameraViewWidget(BaseTabWidget):
 
         self.change_streaming.connect(self.set_streaming)
 
-        # self._connect_toast = Toast(text = 'Attempting to connect to the frame server', duration = 3, parent = self)
-        # self._disconnect_toast = Toast(text = 'Disconnected from the frame server', duration = 3, parent = self)
+        self.video_menu = QtWidgets.QMenu("Video")
 
-    def update_image(self, frame: np.ndarray):
+        self.video_menu_streaming = QtGui.QAction("Stream Video")
+        self.video_menu_streaming.triggered.connect(lambda: self.set_streaming())
+        self.video_menu_streaming.setCheckable(True)
+        self.video_menu.addAction(self.video_menu_streaming)
+
+        self.video_menu_auto = QtGui.QAction("Auto Select")
+        self.video_menu_auto.triggered.connect(self.set_auto_select)
+        self.video_menu_auto.setCheckable(True)
+        self.video_menu_auto.setEnabled(False)
+        self.video_menu.addAction(self.video_menu_auto)
+
+    def update_image(self, frame: np.ndarray) -> None:
         self.view.setPixmap(stream.convert_cv_qt(frame, (self.view.width(), self.view.height())))
 
-    def camera_selected(self, name: str):
+    def set_auto_select(self, enabled: bool) -> None:
+        self.auto_select = enabled
+        self.video_menu_auto.setChecked(enabled)
+        self.auto_select_check.setChecked(enabled)
+        self.camera_picker.setDisabled(enabled)
+        if not enabled:
+            self.set_camera(self.camera_picker.currentText())
+        self.send_message("avr/camera/auto", json.dumps({"enabled": enabled}))
+
+    def set_camera(self, name: str) -> None:
+        if name is None:
+            return
         camera = CAMERAS.get(name, DEFAULT_CAMERA)
         index = camera.get("index", -1)
         if index >= 0:
             self.set_camera_info(camera)
             self.send_message("avr/camera/select", index)
 
-    def set_camera_info(self, camera: dict):
+    def set_camera_info(self, camera: dict) -> None:
         resolution = camera.get("resolution", DEFAULT_CAMERA["resolution"])
         fov = camera.get("fov", DEFAULT_CAMERA["fov"])
         model = camera.get("model", DEFAULT_CAMERA["model"])
@@ -168,8 +206,17 @@ class CameraViewWidget(BaseTabWidget):
         self.fov_text.setText(f"FOV: { fov }º")
         self.model_text.setText(f"Model: { model }")
 
+    @staticmethod
+    def get_camera_name_from_index(index: int) -> str:
+        for name, info in CAMERAS.items():
+            if info.get("index", -1) == index:
+                return name
+        return None
+
     def set_streaming(self, enabled: bool = None) -> bool:
         return_value = False
+        if enabled == self.is_connected:
+            return True
         if enabled is None:
             enabled = not self.is_connected
         if enabled:
@@ -177,25 +224,29 @@ class CameraViewWidget(BaseTabWidget):
             try:
                 port = int(self.port_line_edit.text())
             except ValueError:
-                logger.debug("Invalid port specified")
+                logger.debug("Invalid port or fps specified")
                 return return_value
-            self.is_connected = True
             self.connect_button.setText("Disconnect")
             self.port_line_edit.setReadOnly(True)
-            self.update_thread = Thread(target = lambda: self._update_loop((hostname, port))).start()
+            self.video_menu_streaming.setChecked(True)
+            self.is_connected = True
+            self.update_thread = Thread(target = lambda: self._update_loop((hostname, port)), daemon = True).start()
             self.streaming_changed.emit(True)
             return_value = True
+            logger.info("Streaming started")
         else:
             self.connect_button.setText("Connect")
             self.port_line_edit.setReadOnly(False)
+            self.video_menu_streaming.setChecked(False)
             self.is_connected = False
             self.streaming_changed.emit(False)
             return_value = True
+            logger.info("Streaming stopped")
         self.streaming_text.setText("Streaming: " + str(self.is_connected))
         return return_value
 
-    def _update_loop(self, host):
-        logger.info(f"Socket client connecting to { host[0] }:{ host[1] }")
+    def _update_loop(self, host: tuple[str, int]) -> None:
+        logger.debug(f"Socket client connecting to { host[0] }:{ host[1] }")
         client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, BUFF_SIZE)
         client_socket.sendto(b"connect", host)
@@ -230,7 +281,7 @@ class CameraViewWidget(BaseTabWidget):
                 if not self.shutting_down:
                     self.change_streaming.emit(False)
                 break
-        logger.info("Disconnected socket")
+        logger.debug("Disconnected socket")
         try:
             client_socket.shutdown(socket.SHUT_RDWR)
         except (OSError, TimeoutError):
@@ -239,23 +290,31 @@ class CameraViewWidget(BaseTabWidget):
         # self.send_disconnect_toast.emit()
 
     def process_message(self, topic: str, payload: str) -> None:
-        pass
+        if topic == "avr/camera/auto/select":
+            if self.auto_select:
+                try:
+                    name = self.get_camera_name_from_index(int(payload))
+                    camera = CAMERAS.get(name, DEFAULT_CAMERA)
+                    self.set_camera_info(camera)
+                except ValueError:
+                    pass
 
-    def show_disconnect_toast(self):
-        pass# self._disconnect_toast.show()
-
-    def mqtt_connection_state(self, state: bool):
-        if state:
-            if self.set_streaming(True):
-                pass# self._connect_toast.show()
+    def mqtt_connection_state(self, state: ConnectionState):
+        if state == ConnectionState.connected:
+            if not self.is_connected and self.set_streaming(True):
+                self.toast.show_message("Connected to frame server")
+            self.video_menu_auto.setEnabled(True)
+        else:
+            self.video_menu_auto.setEnabled(False)
 
     def clear(self) -> None:
         self.is_connected = False
         self.connect_button.setText("Connect")
         self.streaming_text.setText("Streaming: " + str(self.is_connected))
 
-    def on_close(self):
+    def closeEvent(self, event: QtGui.QCloseEvent) -> None:
         self.shutting_down = True
         self.is_connected = False
         if self.update_thread is not None:
             self.update_thread.join()
+        return super().closeEvent(event)
